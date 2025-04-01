08ff24d5eb34fd58878e94441591ffc02b578194
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-01 21:09:22 +0800

    Ask the user for input

--- string_manipulation/print_name_in_uppercase_format.py
+++ string_manipulation/print_name_in_uppercase_format.py
@@ -1,4 +1,1 @@
-# Pseudocode
-    # Ask the user to enter their fullname
-    # Use a function to convert the given input to uppercase format
-    # Print the correct output
+full_name = input("Enter your fullname: ")
