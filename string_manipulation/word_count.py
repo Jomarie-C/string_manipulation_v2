bb16038e1772fd023d8dabab9a87470390cce151
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 08:49:19 +0800

    Ask the user to enter a complete statement

--- string_manipulation/word_count.py
+++ string_manipulation/word_count.py
@@ -1,4 +1,1 @@
-# Pseudocode
-    # Ask the user to enter a complete statement
-    # Use a function to count the words in the given statement
-    # Display word count
+statement = input("Enter a complete statement: ")
