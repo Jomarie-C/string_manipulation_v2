47e90e6fe3fe297893ed60a11cbf70c0a032f632
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 08:41:06 +0800

    Ask the user for an input

--- string_manipulation/print_name_in_a_reverse_casing_format.py
+++ string_manipulation/print_name_in_a_reverse_casing_format.py
@@ -1,4 +1,1 @@
-# Pseudocode
-    # Ask the user for an input
-    # Use a function to convert the given input to reverse casing format
-    # Print name in a reverse casing format
+full_name = input("Enter your name: ")
