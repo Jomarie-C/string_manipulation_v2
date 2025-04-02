641116e4fd7d14c814cedff74926d8cdfa29782c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 09:19:19 +0800

    Ask the user for an input

--- string_manipulation/print_name_in_a_snake_casing_format.py
+++ string_manipulation/print_name_in_a_snake_casing_format.py
@@ -1,4 +1,1 @@
-# Pseudocode
-    # Ask the user for an input
-    # Use functions to convert the given input to a snake casing format
-    # Print the name in a snake casing format
+full_name = input("Enter your full name: ")
